f0006c28af415db5827776a6f5a694ba7b6256b1
Dmitry <118049037+mitruee@users.noreply.github.com>
2023-02-19 14:33:57 +0300

    Update BusTicket.java

--- app/src/main/java/com/example/applicationbusticket/BusTicket.java
+++ app/src/main/java/com/example/applicationbusticket/BusTicket.java
@@ -1,36 +1,34 @@
 package com.example.applicationbusticket;
 
 public class BusTicket {
-    // задание полей сущности (её характеристик)
-    private String departurePoint; // место отправления
-    private String arrivalPoint; // место прибытия
+    private String departurePoint;
+    private String arrivalPoint;
     private int groupPensioner;
     private int groupAdult;
     private int groupKids;
-    private String departureDate; // время отправления
-    private String travelTime; // время пути
+    private String departureDate;
+    private String travelTime;
     private int ticketPricePensioner;
     private int ticketPriceAdult;
     private int ticketPriceKids;
 
 
-    // создание конструктора исходных данных
+
     public BusTicket(String departurePoint, String arrivalPoint, int groupPensioner, int groupAdult,
                      int groupKids, String departureDate, String travelTime, int ticketPricePensioner,
                      int ticketPriceAdult, int ticketPriceKids) {
-        this.departurePoint = departurePoint; // место отправления
-        this.arrivalPoint = arrivalPoint; // место прибытия
+        this.departurePoint = departurePoint;
+        this.arrivalPoint = arrivalPoint;
         this.groupPensioner = groupPensioner;
         this.groupAdult = groupAdult;
         this.groupKids = groupKids;
-        this.departureDate = departureDate; // время отправления
-        this.travelTime = travelTime; // время пути
-        this.ticketPricePensioner = ticketPricePensioner; // стоимость билета
+        this.departureDate = departureDate;
+        this.travelTime = travelTime;
+        this.ticketPricePensioner = ticketPricePensioner;
         this.ticketPriceAdult = ticketPriceAdult;
         this.ticketPriceKids = ticketPriceKids;
     }
 
-    // создание геттеров и сеттеров
     public String getDeparturePoint() {
         return departurePoint;
     }
@@ -88,7 +86,6 @@ public class BusTicket {
 
     public void setTicketPriceKids(int ticketPriceKids) { this.ticketPriceKids = ticketPriceKids; }
 
-    // переопределение метода toString()
     @Override
     public String toString() {
         return "Билеты на междугородний автобус в количестве " + (groupPensioner + groupAdult + groupKids) + ":\n" + 
