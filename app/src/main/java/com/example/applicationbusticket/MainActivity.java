048384c6f3f7740aec5af914692dd54109da4629
Dmitry <118049037+mitruee@users.noreply.github.com>
2023-02-19 14:34:30 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/applicationbusticket/MainActivity.java
+++ app/src/main/java/com/example/applicationbusticket/MainActivity.java
@@ -7,24 +7,19 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // создание объекта сущности железнодорожного билета
     BusTicket busTicket = new BusTicket("Горно-Алтайск", "Артыбаш",
             5, 9, 11, "7.00 1 июня",
             "4 часа 30 минут", 49, 70, 35);
 
-    // создание дополнительных полей для вывода на экран полученных значений
-    private TextView busTicketOut; // поле вывода информации о билете
+    private TextView busTicketOut;
 
-    // вывод на экран полученных значений
     @Override
-    protected void onCreate(Bundle savedInstanceState) { // создание жизненного цикла активности
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); // присваивание жизненному циклу активити представления activity_main
+        setContentView(R.layout.activity_main);
 
-        // присваивание переменным активити элементов представления activity_main
-        busTicketOut = findViewById(R.id.busTicketOut); // вывод информации о билете
+        busTicketOut = findViewById(R.id.busTicketOut);
 
-        // заполнение экрана
         busTicketOut.setText(busTicket.toString());
     }
 }
